57e5c53682fcef902838344cbce464ee59ac0c71
Simon Ammer <ammersimon@gmail.com>
2019-05-16 20:40:48 +0200

    Set up project structure

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "com.ammerzon.connect4"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
